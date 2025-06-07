a0c3dc43552f85fcd75d42c3eb5990ec844f94a9
Adebayolusipe <pamilerin.olusipe@gmail.com>
2025-06-07 20:07:34 +0000

    add statc

--- skill_lms/settings.py
+++ skill_lms/settings.py
@@ -121,6 +121,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
+)
 
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
